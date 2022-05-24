65ed6d6b0f682898d3a864bb08ce2ac5f9b28740
kitakou0313 <kitakou0313@gmail.com>
2022-05-24 10:35:34 +0900

    Adjust commands

--- bench/after-bench.sh
+++ bench/after-bench.sh
@@ -6,27 +6,29 @@ set -e
 
 echo "Start to fetch log file from localhost"
 
-source ./hosts/hosts.txt
+# source ./hosts/hosts.txt
 
-for ((host_idx=0; host_idx<${FRONTEND_HOSTS_NUMS}; host_idx++));
-do
-  echo "Fetch log from ${FRONTEND_HOSTS[host_idx]}:${FRONTEND_HOSTS_SSH_PORT[host_idx]}"
-  rsync -e "ssh -p ${FRONTEND_HOSTS_SSH_PORT[host_idx]} -i ${FRONTEND_HOSTS_SSH_PUB_KEY[host_idx]}" -av ${FRONTEND_HOSTS_SSH_USER[host_idx]}@${FRONTEND_HOSTS[host_idx]}:/var/log/nginx/access.log ./kataribe/webserver-log/access.log
-done
+# for ((host_idx=0; host_idx<${FRONTEND_HOSTS_NUMS}; host_idx++));
+# do
+#   echo "Fetch log from ${FRONTEND_HOSTS[host_idx]}:${FRONTEND_HOSTS_SSH_PORT[host_idx]}"
+#   rsync -e "ssh -p ${FRONTEND_HOSTS_SSH_PORT[host_idx]} -i ${FRONTEND_HOSTS_SSH_PUB_KEY[host_idx]}" -av ${FRONTEND_HOSTS_SSH_USER[host_idx]}@${FRONTEND_HOSTS[host_idx]}:/var/log/nginx/access.log ./kataribe/webserver-log/access.log
+# done
 
-for ((host_idx=0; host_idx<${DB_HOSTS_NUMS}; host_idx++));
-do
-  # Fetch mysql slowquery-log
-  echo "Fetch mysql slow query log from ${DB_HOSTS[host_idx]}:${DB_HOSTS_SSH_PORT[host_idx]}"
-  rsync -e "ssh -p ${DB_HOSTS_SSH_PORT[host_idx]} -i ${DB_HOSTS_SSH_PUB_KEY[host_idx]}" -av ${DB_HOSTS_SSH_USER[host_idx]}@${DB_HOSTS[host_idx]}:/var/log/mysql/mysql-slow.log ./mysql-slowquery/mysql-slowquery-log/mysql-slow.log
-done
+# for ((host_idx=0; host_idx<${DB_HOSTS_NUMS}; host_idx++));
+# do
+#   # Fetch mysql slowquery-log
+#   echo "Fetch mysql slow query log from ${DB_HOSTS[host_idx]}:${DB_HOSTS_SSH_PORT[host_idx]}"
+#   rsync -e "ssh -p ${DB_HOSTS_SSH_PORT[host_idx]} -i ${DB_HOSTS_SSH_PUB_KEY[host_idx]}" -av ${DB_HOSTS_SSH_USER[host_idx]}@${DB_HOSTS[host_idx]}:/var/log/mysql/mysql-slow.log ./mysql-slowquery/mysql-slowquery-log/mysql-slow.log
+# done
 
-for ((host_idx=0; host_idx<${APP_HOSTS_NUMS}; host_idx++));
-do 
-  # Fetch pprof logs
-  echo "Fetch pprof log from ${APP_HOSTS[host_idx]}:${APP_HOSTS_SSH_PORT[host_idx]}"
-  rsync -e "ssh -p ${APP_HOSTS_SSH_PORT[host_idx]} -i ${APP_HOSTS_SSH_PUB_KEY[host_idx]}" -av ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]}:/root/webapp/cpu.pprof ./pprof/profilefiles/cpu.pprof
+# for ((host_idx=0; host_idx<${APP_HOSTS_NUMS}; host_idx++));
+# do 
+#   # Fetch pprof logs
+#   echo "Fetch pprof log from ${APP_HOSTS[host_idx]}:${APP_HOSTS_SSH_PORT[host_idx]}"
+#   rsync -e "ssh -p ${APP_HOSTS_SSH_PORT[host_idx]} -i ${APP_HOSTS_SSH_PUB_KEY[host_idx]}" -av ${APP_HOSTS_SSH_USER[host_idx]}@${APP_HOSTS[host_idx]}:/root/webapp/cpu.pprof ./pprof/profilefiles/cpu.pprof
+# done
 
-done
+rsync -e -av /home/kitakou/kayac-isucon/code/kayac-isucon-2022-main/webapp/nginx/log/access.log ./kataribe/webserver-log/access.log
+rsync -e -av /home/kitakou/kayac-isucon/code/kayac-isucon-2022-main/webapp/mysql/logs/mysql-slow.log ./mysql-slowquery/mysql-slowquery-log/mysql-slow.log
 
 echo "Finish to fetch log files"
--- bench/before-bench.sh
+++ bench/before-bench.sh
@@ -7,19 +7,23 @@ set -e
 
 echo "Start to clean logs in remote hosts"
 
-source ./hosts/hosts.txt
+# source ./hosts/hosts.txt
 
-for ((host_idx=0; host_idx<${FRONTEND_HOSTS_NUMS}; host_idx++));
-do
-  echo "Backup log and clean log in ${FRONTEND_HOSTS[host_idx]}:${FRONTEND_HOSTS_SSH_PORT[host_idx]}"
-  ssh -p ${FRONTEND_HOSTS_SSH_PORT[host_idx]} -i ${FRONTEND_HOSTS_SSH_PUB_KEY[host_idx]} ${FRONTEND_HOSTS_SSH_USER[host_idx]}@${FRONTEND_HOSTS[host_idx]} 'bash -s ' < ./bench/utils/nginx-backup-clean-log.sh
-done
+# for ((host_idx=0; host_idx<${FRONTEND_HOSTS_NUMS}; host_idx++));
+# do
+#   echo "Backup log and clean log in ${FRONTEND_HOSTS[host_idx]}:${FRONTEND_HOSTS_SSH_PORT[host_idx]}"
+#   ssh -p ${FRONTEND_HOSTS_SSH_PORT[host_idx]} -i ${FRONTEND_HOSTS_SSH_PUB_KEY[host_idx]} ${FRONTEND_HOSTS_SSH_USER[host_idx]}@${FRONTEND_HOSTS[host_idx]} 'bash -s ' < ./bench/utils/nginx-backup-clean-log.sh
+# done
 
 
-for ((host_idx=0; host_idx<${DB_HOSTS_NUMS}; host_idx++));
-do
-  echo "Backup log and clean log in ${DB_HOSTS[host_idx]}:${DB_HOSTS_SSH_PORT[host_idx]}"
-  ssh -p ${DB_HOSTS_SSH_PORT[host_idx]} -i ${DB_HOSTS_SSH_PUB_KEY[host_idx]} ${DB_HOSTS_SSH_USER[host_idx]}@${DB_HOSTS[host_idx]} 'bash -s ' < ./bench/utils/slowquery-backup-clean-log.sh
-done
+# for ((host_idx=0; host_idx<${DB_HOSTS_NUMS}; host_idx++));
+# do
+#   echo "Backup log and clean log in ${DB_HOSTS[host_idx]}:${DB_HOSTS_SSH_PORT[host_idx]}"
+#   ssh -p ${DB_HOSTS_SSH_PORT[host_idx]} -i ${DB_HOSTS_SSH_PUB_KEY[host_idx]} ${DB_HOSTS_SSH_USER[host_idx]}@${DB_HOSTS[host_idx]} 'bash -s ' < ./bench/utils/slowquery-backup-clean-log.sh
+# done
+
+cat /dev/null > /home/kitakou/kayac-isucon/code/kayac-isucon-2022-main/webapp/nginx/log/access.log
+cat /dev/null > /home/kitakou/kayac-isucon/code/kayac-isucon-2022-main/webapp/mysql/logs/mysql-slow.log
+
 
 echo "Finish to backup log and clean log"
