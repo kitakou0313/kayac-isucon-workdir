2a3d453499bdc676882c55b47bb9fe935f3b5228
kitakou0313 <kitakou0313@gmail.com>
2022-06-01 09:33:22 +0900

    Add index to playlist table

--- webapp/sql/50_listen80_schema.sql
+++ webapp/sql/50_listen80_schema.sql
@@ -40,6 +40,8 @@ CREATE TABLE `playlist` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 CREATE INDEX ulid_index ON isucon_listen80.playlist (ulid);
+CREATE INDEX is_pub_created_at ON isucon_listen80.playlist (is_public, created_at);
+
 
 CREATE TABLE `playlist_song` (
   `playlist_id` BIGINT NOT NULL,
