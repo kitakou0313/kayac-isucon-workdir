351aabe5b76edf7163170a2425a72162648f6924
kitakou0313 <kitakou0313@gmail.com>
2022-06-02 10:39:27 +0900

    Add init script to add index

---
+++ webapp/sql/99_create_index.sql
@@ -0,0 +1,15 @@
+-- Song table index
+CREATE INDEX ulid_index ON isucon_listen80.song (ulid);
+
+-- playlist index
+CREATE INDEX ulid_index ON isucon_listen80.playlist (ulid);
+CREATE INDEX is_pub_created_at ON isucon_listen80.playlist (is_public, created_at);
+
+-- playlist_song index
+CREATE INDEX playlist_id_idx ON isucon_listen80.playlist_song (playlist_id);
+
+-- playlist_favorite index
+CREATE INDEX playlist_id_idx ON isucon_listen80.playlist_favorite (playlist_id);
+CREATE INDEX playlist_id_user_acc_idx ON isucon_listen80.playlist_favorite (playlist_id,favorite_user_account);
+CREATE INDEX user_acc__created_idx ON isucon_listen80.playlist_favorite (favorite_user_account, created_at);
+
