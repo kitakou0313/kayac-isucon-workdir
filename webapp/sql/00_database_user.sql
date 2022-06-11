a0ba22779446463e5acab76665383982f9c0f1a2
kitakou0313 <kitakou0313@gmail.com>
2022-06-11 12:01:45 +0900

    Add Process role

--- webapp/sql/00_database_user.sql
+++ webapp/sql/00_database_user.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS `isucon_listen80`;
 CREATE USER isucon IDENTIFIED BY 'isucon';
 GRANT ALL PRIVILEGES ON isucon_listen80.* TO 'isucon'@'%';
+GRANT PROCESS ON *.* TO 'isucon'@'%';
 
 SET PERSIST local_infile=1;
