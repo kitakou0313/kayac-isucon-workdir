cf47f68c80b5479f8dfe93f775f8758d2b100154
kitakou0313 <kitakou0313@gmail.com>
2022-06-16 10:39:39 +0900

    Add index

--- webapp/sql/99_create_index.sql
+++ webapp/sql/99_create_index.sql
@@ -6,6 +6,8 @@ CREATE INDEX ulid_index ON isucon_listen80.playlist (ulid);
 CREATE INDEX is_pub_created_at ON isucon_listen80.playlist (is_public, created_at);
 CREATE INDEX is_pub_fav_cnt ON isucon_listen80.playlist (is_public, fav_cnt);
 CREATE INDEX user_acc_created ON isucon_listen80.playlist (user_account, created_at);
+CREATE INDEX is_pub_id ON isucon_listen80.playlist (is_pub, id);
+
 
 -- user index
 CREATE INDEX is_ban_account ON isucon_listen80.user (is_ban, account);
